c785edf8147cb5a72cae09df6d96fda4c8e50410
drknsblct <57183092+drknsblct@users.noreply.github.com>
2021-09-18 02:41:13 +0300

    Update exercise.py

--- src/exercise.py
+++ src/exercise.py
@@ -4,7 +4,7 @@ def main():
     num2 = int(input(''))
     
     if num1 > num2:
-        print(str(num1) + ' is bigger than ' + str(num2) + '.')
+        print(str(num1) + ' is greater than ' + str(num2) + '.')
     elif num1 < num2:
         print(str(num1) + ' is smaller than ' + str(num2) + '.')
     else:
